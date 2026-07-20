5d47626a2c0633e022a426c5736d1f7d2543fbd1
unknown <bhavya.t2024@gmail.com>
2026-07-20 00:48:08 -0400

    deploy

--- back.py
+++ back.py
@@ -19,148 +19,145 @@ app = Flask(__name__)
 #app2 = Flask(__name__)
 CORS(app)
 #CORS(app2)
-#AHHH model = SentenceTransformer("all-MiniLM-L6-v2")
-#AHHHHH
-#@app.route("/queries", methods=["POST"])
-#def queries():
-    #AHHHH
-    # pdf = request.files["pdf"]
-    # user_id = request.form["user_id"]    
+@app.route("/queries", methods=["POST"])
+def queries():
+    pdf = request.files["pdf"]
+    user_id = request.form["user_id"]    
     
-    # load_dotenv()
-    # api_key = os.getenv("OPENROUTER_API_KEY")
+    load_dotenv()
+    api_key = os.getenv("OPENROUTER_API_KEY")
 
-    # sys.stdout.reconfigure(encoding='utf-8')
+    sys.stdout.reconfigure(encoding='utf-8')
 
-    # doc = pymupdf.open(stream=pdf.read(), filetype="pdf")
-    # metadata = doc.metadata
+    doc = pymupdf.open(stream=pdf.read(), filetype="pdf")
+    metadata = doc.metadata
 
-    # paper_title = metadata.get("title", "")
-    # paper_author = metadata.get("author", "")
-    # paper_subject = metadata.get("subject", "")
+    paper_title = metadata.get("title", "")
+    paper_author = metadata.get("author", "")
+    paper_subject = metadata.get("subject", "")
 
-    # first_page = doc.load_page(0)
+    first_page = doc.load_page(0)
 
-    # first_page_text = first_page.get_text("text")
-
-
-    # paper_info = f"""
-
-    # Paper Metadata:
-
-    # Title:
-    # {paper_title}
-
-    # Authors:
-    # {paper_author}
-
-    # Subject:
-    # {paper_subject}
+    first_page_text = first_page.get_text("text")
 
 
-    # Beginning of paper:
-    # {first_page_text[:2000]}
+    paper_info = f"""
 
-    # """
+    Paper Metadata:
 
-    # box = pymupdf.Rect(0, 100, 612, 792)
-    # text = ""
+    Title:
+    {paper_title}
 
-    # page1 = doc.load_page(0)
+    Authors:
+    {paper_author}
 
-    # abstract = page1.search_for("abstract")
-    # if abstract:
-    #     box1 = pymupdf.Rect(abstract[0].x0, abstract[0].y0, 612, 792)
-    #     page1 = page1.get_text("text", clip=box1)
-    #     text += page1
+    Subject:
+    {paper_subject}
+
+
+    Beginning of paper:
+    {first_page_text[:2000]}
+
+    """
+
+    box = pymupdf.Rect(0, 100, 612, 792)
+    text = ""
+
+    page1 = doc.load_page(0)
+
+    abstract = page1.search_for("abstract")
+    if abstract:
+        box1 = pymupdf.Rect(abstract[0].x0, abstract[0].y0, 612, 792)
+        page1 = page1.get_text("text", clip=box1)
+        text += page1
         
-    # text_temp = text
-    # line = []
-    # chunks = []
-    # chunk = ""
-    # info = ""
+    text_temp = text
+    line = []
+    chunks = []
+    chunk = ""
+    info = ""
 
-    # counter = 1
-    # for page in doc:
-    #     if (counter >= len(doc)):
-    #         break
-    # page = doc.load_page(counter)
-    # text += page.get_text("text", clip=box)
-    # text_temp += page.get_text("text", clip=box)
-    # line = text_temp.splitlines()
-    # info = page.get_text("dict")
+    counter = 1
+    for page in doc:
+        if (counter >= len(doc)):
+            break
+    page = doc.load_page(counter)
+    text += page.get_text("text", clip=box)
+    text_temp += page.get_text("text", clip=box)
+    line = text_temp.splitlines()
+    info = page.get_text("dict")
 
-    # for lin in line:
-    #     if (re.fullmatch(r'[A-Z][A-Za-z\s&*0-9.-]+', lin)):
-    #         chunks.append(lin)
-    #     elif (re.fullmatch(r'[A-Z][A-Za-z\s&*0-9.()-]+', lin)):
-    #         if (re.search(r'[(][a-zA-Z0-9]+[)][\s][A-Z]', lin)):
-    #             chunks.append(lin)            
-    #     else:
-    #         chunk += lin
-    #         chunk = ""
-    # counter += 1
-    # text_temp = ""
+    for lin in line:
+        if (re.fullmatch(r'[A-Z][A-Za-z\s&*0-9.-]+', lin)):
+            chunks.append(lin)
+        elif (re.fullmatch(r'[A-Z][A-Za-z\s&*0-9.()-]+', lin)):
+            if (re.search(r'[(][a-zA-Z0-9]+[)][\s][A-Z]', lin)):
+                chunks.append(lin)            
+        else:
+            chunk += lin
+            chunk = ""
+    counter += 1
+    text_temp = ""
 
-    # for chunk in chunks:
-    #     if (len(chunk) >= 100):
-    #         chunks.remove(chunk)
+    for chunk in chunks:
+        if (len(chunk) >= 100):
+            chunks.remove(chunk)
 
-    # pattern = r'[a-zA-Z0-9]+@gmail\.com|[a-zA-Z0-9]+@yahoo.com|[a-zA-Z0-9]+@hotmail.com|email[:]+|id:[" "0-9]+|@[a-zA-Z0-9]+'
-    # text = re.sub(pattern, "", text)
+    pattern = r'[a-zA-Z0-9]+@gmail\.com|[a-zA-Z0-9]+@yahoo.com|[a-zA-Z0-9]+@hotmail.com|email[:]+|id:[" "0-9]+|@[a-zA-Z0-9]+'
+    text = re.sub(pattern, "", text)
 
-    # url_pattern = r'https?://[^\s<>"]+|www\.[^\s<>"]+|�'
-    # urls = re.findall(url_pattern, text) # should we create a separate chunk?
-    # text = re.sub(url_pattern, "", text)
+    url_pattern = r'https?://[^\s<>"]+|www\.[^\s<>"]+|�'
+    urls = re.findall(url_pattern, text) # should we create a separate chunk?
+    text = re.sub(url_pattern, "", text)
 
-    # url_patterns = r'[^a-zA-Z0-9\s.,;:()\-\!?"\'/@+=]'
-    # text = re.sub(url_patterns, "", text)
+    url_patterns = r'[^a-zA-Z0-9\s.,;:()\-\!?"\'/@+=]'
+    text = re.sub(url_patterns, "", text)
 
-    # fullText = []
-    # text_temp = ""
-    # for char in text:
-    #     if (char != " "):
-    #         text_temp += char
-    #     else:
-    #         text_temp += char
-    #         fullText.append(text_temp)
-    #         text_temp = ""
+    fullText = []
+    text_temp = ""
+    for char in text:
+        if (char != " "):
+            text_temp += char
+        else:
+            text_temp += char
+            fullText.append(text_temp)
+            text_temp = ""
 
-    # chunks = [chunk.strip() for chunk in chunks]
-    # fullText = [word.strip() for word in fullText]
+    chunks = [chunk.strip() for chunk in chunks]
+    fullText = [word.strip() for word in fullText]
 
-    # pieces = []
-    # piece = ""
-    # counter = 0
-    # word_c = 0
+    pieces = []
+    piece = ""
+    counter = 0
+    word_c = 0
 
-    # print(chunks[1])
-    # print(fullText[164])
+    print(chunks[1])
+    print(fullText[164])
 
-    # pattern = "|".join(re.escape(chunk) for chunk in chunks)
-    # pieces = re.split(pattern, text)
+    pattern = "|".join(re.escape(chunk) for chunk in chunks)
+    pieces = re.split(pattern, text)
 
-    # query = "Represent this sentence for searhching relevant passages: " + "What is the impact of artificial intelligence on the business industry?"
-    # user_query = request.form["question"]
-    # document = pieces
+    model = SentenceTransformer("all-MiniLM-L6-v2")
+    query = "Represent this sentence for searhching relevant passages: " + "What is the impact of artificial intelligence on the business industry?"
+    user_query = request.form["question"]
+    document = pieces
 
-    # queryEmbed = model.encode(query, normalize_embeddings=True)
-    # documentEmbed = model.encode(document, normalize_embeddings=True)
+    queryEmbed = model.encode(query, normalize_embeddings=True)
+    documentEmbed = model.encode(document, normalize_embeddings=True)
 
-    # scores = documentEmbed @ queryEmbed
-    # top_k = 3
+    scores = documentEmbed @ queryEmbed
+    top_k = 3
 
-    # indices = numpy.argsort(scores)
-    # indices = indices[::-1]
-    # indices = indices[:top_k]
+    indices = numpy.argsort(scores)
+    indices = indices[::-1]
+    indices = indices[:top_k]
 
-    # final_text = ""
+    final_text = ""
 
-    # for i in indices:
-    #     final_text += pieces[i]
-    #     print(pieces[i])
-    #     print(scores[i])
-    #AHHHHH
+    for i in indices:
+        final_text += pieces[i]
+        print(pieces[i])
+        print(scores[i])
 
     #client = OpenAI(
     #  base_url="https://openrouter.ai/api/v1",
@@ -200,14 +197,12 @@ CORS(app)
     #  messages=messages,
     #  extra_body={"reasoning": {"enabled": True}}
     #)
-    #AHHHH
-    # from openai import OpenAI
+    from openai import OpenAI
 
-    # client = OpenAI(
-    # base_url="https://openrouter.ai/api/v1",
-    # api_key=api_key,
-    # )
-    #AHHHHH
+    client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=api_key,
+    )
 
     # First API call with reasoning
     # response = client.chat.completions.create(
@@ -220,114 +215,111 @@ CORS(app)
     #     ],
     # extra_body={"reasoning": {"enabled": True}}
     # )
-    #AHHH
 
-    # response = client.chat.completions.create(
+    response = client.chat.completions.create(
 
-    # model="openrouter/free",
+    model="openrouter/free",
 
-    # messages=[
+    messages=[
 
-    # {
-    #     "role":"system",
+    {
+        "role":"system",
 
-    #     "content":
-    #         """
-    #         You are an academic research assistant.
+        "content":
+            """
+            You are an academic research assistant.
 
-    #         Answer the user's question using only the research paper.
+            Answer the user's question using only the research paper.
 
-    #         Return ONLY JSON in this format:
+            Return ONLY JSON in this format:
 
-    #         {
-    #         "answer":"AI answer here",
-    #         "citation":"MLA citation here"
-    #         }
-
-
-    #         Create an MLA citation using:
-    #         - authors
-    #         - paper title
-    #         - journal/conference
-    #         - year
-    #         - DOI if available
-
-    #         Do not invent information.
-    #         If information is missing, leave that part blank.
-
-    #         """
-    #             },
+            {
+            "answer":"AI answer here",
+            "citation":"MLA citation here"
+            }
 
 
-    #             {
-    #                 "role":"user",
+            Create an MLA citation using:
+            - authors
+            - paper title
+            - journal/conference
+            - year
+            - DOI if available
 
-    #                 "content":
+            Do not invent information.
+            If information is missing, leave that part blank.
 
-    #                 paper_info
-
-    #                 +
-
-    #                 """
-
-    #         Relevant paper sections:
-
-    #         """
-
-    #                 +
-
-    #                 final_text
-
-    #                 +
-
-    #                 """
-
-    #         Question:
-
-    #         """
-
-    #                 +
-
-    #                 user_query
-
-    #             }
-
-    #             ],
-
-    #             extra_body={
-    #                 "reasoning":{
-    #                     "enabled":True
-    #                 }
-    #             }
-
-    #         )
+            """
+                },
 
 
-    # response_text = response.choices[0].message.content
+                {
+                    "role":"user",
+
+                    "content":
+
+                    paper_info
+
+                    +
+
+                    """
+
+            Relevant paper sections:
+
+            """
+
+                    +
+
+                    final_text
+
+                    +
+
+                    """
+
+            Question:
+
+            """
+
+                    +
+
+                    user_query
+
+                }
+
+                ],
+
+                extra_body={
+                    "reasoning":{
+                        "enabled":True
+                    }
+                }
+
+            )
 
 
-    # response_data = json.loads(response_text)
+    response_text = response.choices[0].message.content
 
 
-    # ai_answer = response_data["answer"]
+    response_data = json.loads(response_text)
 
-    # mla_citation = response_data["citation"]
 
-    # return {
+    ai_answer = response_data["answer"]
 
-    #     "status":"success",
+    mla_citation = response_data["citation"]
 
-    #     "response":ai_answer,
+    return {
 
-    #     "citations":[
-    #         mla_citation
-    #     ],
+        "status":"success",
 
-    #     "context":final_text
+        "response":ai_answer,
 
-    # }
+        "citations":[
+            mla_citation
+        ],
 
-    #AHHHH
+        "context":final_text
+
+    }
 
 #     return {
 #         "status": "success",
@@ -335,7 +327,6 @@ CORS(app)
 #         "citations": urls,
 #         "context": final_text
 # }
-#AHHHHHH
 
 
     # SUPABASE_URL = 'https://yrybsyvpiorvidpouurv.supabase.co'
@@ -374,67 +365,65 @@ CORS(app)
 
 # @app.route("/signup")
 # def signup():
-#AHHHHH
-# @app.route("/followup", methods=["POST"])
-# def followup():
+@app.route("/followup", methods=["POST"])
+def followup():
 
-#     data = request.json
+    data = request.json
 
-#     question = data["question"]
-#     context = data["context"]
-
-
-#     load_dotenv()
-
-#     api_key = os.getenv("OPENROUTER_API_KEY")
+    question = data["question"]
+    context = data["context"]
 
 
-#     client = OpenAI(
-#         base_url="https://openrouter.ai/api/v1",
-#         api_key=api_key
-#     )
+    load_dotenv()
+
+    api_key = os.getenv("OPENROUTER_API_KEY")
 
 
-#     response = client.chat.completions.create(
-#         model="openrouter/free",
-
-#         response_format={
-#             "type": "json_object"
-#         },
-
-#         messages=[
-#             {
-#                 "role":"system",
-#                 "content":
-#                 """
-#                 Answer the user's question using only the provided research paper context.
-#                 Rules:
-#                 - Use ONLY the research paper context.
-#                 - Do NOT return JSON.
-#                 - Do NOT use markdown code blocks.
-#                 - Do NOT create dictionaries or lists with keys.
-#                 - Explain concepts clearly in paragraphs.
-#                 - ONLY ANSWER IN ENGLISH.
-
-#                 Research paper context:
-#                 """
-#                 +
-#                 context
-#             },
-
-#             {
-#                 "role":"user",
-#                 "content":question
-#             }
-#         ]
-#     )
+    client = OpenAI(
+        base_url="https://openrouter.ai/api/v1",
+        api_key=api_key
+    )
 
 
-#     return {
-#         "response":
-#         response.choices[0].message.content
-#     }
-#AHHHH
+    response = client.chat.completions.create(
+        model="openrouter/free",
+
+        response_format={
+            "type": "json_object"
+        },
+
+        messages=[
+            {
+                "role":"system",
+                "content":
+                """
+                Answer the user's question using only the provided research paper context.
+                Rules:
+                - Use ONLY the research paper context.
+                - Do NOT return JSON.
+                - Do NOT use markdown code blocks.
+                - Do NOT create dictionaries or lists with keys.
+                - Explain concepts clearly in paragraphs.
+                - ONLY ANSWER IN ENGLISH.
+
+                Research paper context:
+                """
+                +
+                context
+            },
+
+            {
+                "role":"user",
+                "content":question
+            }
+        ]
+    )
+
+
+    return {
+        "response":
+        response.choices[0].message.content
+    }
 #@app.route("/features")
 #def features():
 #    return render_template("features.html")
